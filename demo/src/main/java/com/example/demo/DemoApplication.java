630bd4d8fbb29af0d3e64b17bec439a873e8c698
ranjithbillakanti522 <144865960+ranjithbillakanti522@users.noreply.github.com>
2023-09-12 22:24:34 -0400

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+//import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	
+	;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		
+	}
+
+}
